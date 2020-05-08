4d13dc458bd0500eaf30be25a0d3d1fdf6eb0e02
Kaustav Chanda <kstvkmrchanda2@gmail.com>
2020-05-09 01:39:09 +0530

    layout added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-
-import Navbar from './components/Navbar'
+import { Container, Row, Col } from "react-bootstrap";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
-    <div>
-      <Navbar></Navbar>  
-    </div>
+    <>
+      <Navbar></Navbar>
+      <Container style={{ marginTop : "6em"}}>
+        <Row className="justify-content-md-center">
+          <Col xs lg="8">
+            <Home></Home>
+          </Col>
+          <Col xs lg="4">
+            <Sidebar></Sidebar>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
 
