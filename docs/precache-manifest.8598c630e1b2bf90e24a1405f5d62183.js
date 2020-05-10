c16daf2ac966a88275c5db73d96ac1f9cf41128d
Kaustav Chanda <kstvkmrchanda2@gmail.com>
2020-05-10 16:10:54 +0530

    corrected paths for deployment

---
+++ docs/precache-manifest.8598c630e1b2bf90e24a1405f5d62183.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc4480af0e9508c0d5cf2def08bdccc8",
+    "url": "/shutterbug/index.html"
+  },
+  {
+    "revision": "57925f52c3468a0c2749",
+    "url": "/shutterbug/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "92461103be487073f7a4",
+    "url": "/shutterbug/static/css/main.04c779fc.chunk.css"
+  },
+  {
+    "revision": "57925f52c3468a0c2749",
+    "url": "/shutterbug/static/js/2.a29b65b3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/shutterbug/static/js/2.a29b65b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92461103be487073f7a4",
+    "url": "/shutterbug/static/js/main.862cb044.chunk.js"
+  },
+  {
+    "revision": "cc5f761d088dd01b84ff",
+    "url": "/shutterbug/static/js/runtime-main.6307b178.js"
+  },
+  {
+    "revision": "ce7f9e23cbeeb2ced933dfe547712b7a",
+    "url": "/shutterbug/static/media/24.ce7f9e23.jpg"
+  },
+  {
+    "revision": "64a02404402db81031b9b6675d4b52ea",
+    "url": "/shutterbug/static/media/47.64a02404.jpg"
+  },
+  {
+    "revision": "f5837ba3592a8cea704f6a57ad3dec26",
+    "url": "/shutterbug/static/media/53.f5837ba3.jpg"
+  },
+  {
+    "revision": "3397c241c9a2326ed762e966fb600d76",
+    "url": "/shutterbug/static/media/63.3397c241.jpg"
+  },
+  {
+    "revision": "63ee76f2b1e49cb2738393115774ff53",
+    "url": "/shutterbug/static/media/71.63ee76f2.jpg"
+  },
+  {
+    "revision": "b514813864c467e8fa1a5e4cd377a308",
+    "url": "/shutterbug/static/media/74.b5148138.jpg"
+  },
+  {
+    "revision": "476eaa7cfd615adb34cb1e68129b5943",
+    "url": "/shutterbug/static/media/81.476eaa7c.jpg"
+  },
+  {
+    "revision": "d5dd30b5fafee5f9b46cadeb3028afa4",
+    "url": "/shutterbug/static/media/alexs.d5dd30b5.png"
+  },
+  {
+    "revision": "dd616cdb9edc29535ecc5093c7d89d34",
+    "url": "/shutterbug/static/media/fanette.dd616cdb.png"
+  },
+  {
+    "revision": "4ec201c129bd802b50730fab9ef2c81d",
+    "url": "/shutterbug/static/media/gabe.4ec201c1.png"
+  },
+  {
+    "revision": "de1b0a3a76523d1bbe7b291cc2a96190",
+    "url": "/shutterbug/static/media/honza.de1b0a3a.png"
+  },
+  {
+    "revision": "bbd85e598965d59d5436c39230152967",
+    "url": "/shutterbug/static/media/jeremy.bbd85e59.png"
+  },
+  {
+    "revision": "5efa5a0a75c763837460b26126653883",
+    "url": "/shutterbug/static/media/julien.5efa5a0a.png"
+  },
+  {
+    "revision": "316cee43b6ff5bfc13d9a1554ef421db",
+    "url": "/shutterbug/static/media/lucas.316cee43.png"
+  },
+  {
+    "revision": "d051316a9b0bdfdc11dd97a50275b00c",
+    "url": "/shutterbug/static/media/olivier.d051316a.png"
+  },
+  {
+    "revision": "b927791bc3eedfc238c1a454051ae5be",
+    "url": "/shutterbug/static/media/pragati.b927791b.png"
+  },
+  {
+    "revision": "9f9d5554c7f6d31ef66806615ddc252d",
+    "url": "/shutterbug/static/media/raymond.9f9d5554.png"
+  },
+  {
+    "revision": "9dc9ce33bc6dded75b465a9f41cdbc25",
+    "url": "/shutterbug/static/media/tim.9dc9ce33.png"
+  },
+  {
+    "revision": "3901127349a8711cd7e073848766fd5f",
+    "url": "/shutterbug/static/media/xander.39011273.png"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a4d0fa3493ed4e43ab340d1a6202a0ce.js"
+  "/shutterbug/precache-manifest.8598c630e1b2bf90e24a1405f5d62183.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/shutterbug/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
